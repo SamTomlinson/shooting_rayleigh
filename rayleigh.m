50d38896bd5d1e0a954b3c85899aa0f16d600b1f
Sam Tomlinson <samtomlinson@dyn1231-145.wlan.ic.ac.uk>
2017-07-05 16:14:04 +0100

    Working code with phase speed but oscillatory

--- rayleigh.m
+++ rayleigh.m
@@ -1,6 +1,6 @@
 function vecq = rayleigh(~,q,baseT,~,baseU,baseUdash,alpha,ktilde,M,c)
     vecq(1) = q(2);
-    vecq(2) = -2*baseUdash*q(2)/(baseU-0*c) ...
-        + ktilde^2*baseT*(baseT-alpha^2*M^2*(baseU-0*c)^2/ktilde^2)*q(1);
+    vecq(2) = 2*baseUdash*q(2)/(baseU-c) ...
+        + ktilde^2*baseT*(baseT-alpha^2*M^2*(baseU-c)^2/ktilde^2)*q(1);
     
     
